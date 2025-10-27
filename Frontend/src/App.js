3d2790d3c8bc77770d2cf800763fa6760a1b035a
Nicolas Romano <116515477+nicolaspromano@users.noreply.github.com>
2025-10-27 16:20:09 -0300

    Update App.js
    Adição da rota para pagina de registro

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Páginas Principais e de Login
 import Home from './pages/Home';
 import LoginSelection from './pages/login/LoginSelection';
+import Register from './pages/register/Register';
 
 // Páginas do Professor
 import ProfessorHome from './pages/professor/ProfessorHome';
@@ -15,11 +16,8 @@ import AlunoHome from './pages/aluno/AlunoHome';
 import BancoQuestoesAluno from './pages/aluno/BancoQuestoesAluno';
 
 // Páginas do Fórum
-import ForumDuvidasAluno from './pages/forumAluno/ForumDuvidasAluno';
-import TopicoDetalheAluno from './pages/forumAluno/TopicoDetalheAluno';
-
-import ForumDuvidasProfessor from './pages/forumProfessor/ForumDuvidasProfessor';
-import TopicoDetalheProfessor from './pages/forumProfessor/TopicoDetalheProfessor';
+import ForumDuvidas from './pages/forum/ForumDuvidas';
+import TopicoDetalhe from './pages/forum/TopicoDetalhe';
 
 function App() {
   return (
@@ -31,23 +29,24 @@ function App() {
         {/* Rota para a tela de seleção de Login */}
         <Route path="/login-selection" element={<LoginSelection />} />
 
+        {/* Rota para a tela de seleção de Register */}
+        <Route path="/register" element={<Register />} />
+
         {/* Rotas do Professor */}
         <Route path="/professor/home" element={<ProfessorHome />} />
         <Route path="/professor/perfil" element={<ProfessorPerfil />} />
         <Route path="/professor/banco-questoes" element={<BancoQuestoesProfessor />} />
         <Route path="/professor/provas" element={<SistemaProvas />} />
-        <Route path="/professor/forumProfessor" element={<ForumDuvidasProfessor />} />
+        <Route path="/professor/forum" element={<ForumDuvidas />} />
 
          {/* Rotas do Aluno */}
         <Route path="/aluno/home" element={<AlunoHome />} /> 
         <Route path="/aluno/banco-questoes" element={<BancoQuestoesAluno />} />
-        <Route path="/aluno/forumAluno" element={<ForumDuvidasAluno />} />
+        <Route path="/aluno/forum" element={<ForumDuvidas />} />
 
-        {/* Rotas do Fórum  */}
-        <Route path="/forumAluno" element={<ForumDuvidasAluno />} />
-        <Route path="/forum/topico/:id" element={<TopicoDetalheAluno />} />
-        <Route path="/forumProfessor" element={<ForumDuvidasProfessor />} />
-        <Route path="/forum/topico/:id" element={<TopicoDetalheProfessor />} />
+        {/* Rotas do Fórum (acessíveis de forma geral) */}
+        <Route path="/forum" element={<ForumDuvidas />} />
+        <Route path="/forum/topico/:id" element={<TopicoDetalhe />} />
       </Routes>
     </Router>
   );
